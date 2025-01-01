bba631db993d5034a4a8d2e40ce399940c9116c2
alam117 <99356135+alam117@users.noreply.github.com>
2024-12-31 17:50:06 -0700

    primer commit

---
+++ API.furnitureStore/API.furnitureStore/API.furnitureStore.shared/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.furnitureStore.shared
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public DateTime birthdate { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+    }
+}
