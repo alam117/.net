1bfc7530355789f339489a0c022673d7b9242d03
alam117 <99356135+alam117@users.noreply.github.com>
2025-01-01 17:59:25 -0700

    Create APIFurnitureStoreContext.cs

---
+++ API.furnitureStore/API.furnitureStore/API.furnitureStore.Data/APIFurnitureStoreContext.cs
@@ -0,0 +1,19 @@
+﻿using API.furnitureStore.shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.furnitureStore.Data
+{
+    internal class APIFurnitureStoreContext : DbContext
+    {
+        public APIFurnitureStoreContext(DbContextOptions options):base(options) { }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<productCategory> ProductCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite();
+        }
+    }
+}
